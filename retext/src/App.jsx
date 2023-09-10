8a5b8606d6d8f45519deba30531cd2dbab284768
Hetarth Mahida <hetarthmahida@gmail.com>
2023-09-10 02:51:50 -0400

    Frontend done

--- retext/src/App.jsx
+++ retext/src/App.jsx
@@ -1,24 +1,154 @@
 import './App.css'
+import React,{ useState, useEffect } from "react";
+import Clipboard from 'react-clipboard-animation'
 
 function App() {
+  // All States
+  const [copied1, setCopied1] = useState(false)
+  const [copied2, setCopied2] = useState(false)
+  const [copied3, setCopied3] = useState(false)
+  const [rectified, setRectified] = useState("")
+  const [concise, setConcise] = useState("")
+  const [verbose, setVerbose] = useState("")
 
- 
+  function handleClick() {
+    if (window.getSelection) {
+
+      const highlightedText = window.getSelection().toString()
+
+      console.log(highlightedText)
+
+      axios.post("http://127.0.0.1:8000/rectify", { sentence: highlightedText })
+           .then(res => setRectified(res.data.completion))
+
+      axios.post("http://127.0.0.1:8000/concise", { sentence: highlightedText })
+           .then(res => setConcise(res.data.completion))
+
+      axios.post("http://127.0.0.1:8000/verbose", { sentence: highlightedText })
+           .then(res => setVerbose(res.data.completion))
+
+      axios.post("http://127.0.0.1:8000/formalize", { sentence: highlightedText })
+           .then(res => setFormal(res.data.completion))
+
+    }
+  }
+
+  useEffect(() => {
+    const timeout1 = setTimeout(() => {
+      if (copied1) setCopied1(false)
+    }, 1000)
+
+    return () => clearTimeout(timeout1)
+  }, [copied1])
+
+
+  useEffect(() => {
+    const timeout2 = setTimeout(() => {
+      if (copied2) setCopied2(false)
+    }, 1000)
+
+    return () => clearTimeout(timeout2)
+  }, [copied2])
+  
+
+  useEffect(() => {
+    const timeout3 = setTimeout(() => {
+      if (copied3) setCopied3(false)
+    }, 1000)
+
+    return () => clearTimeout(timeout3)
+  }, [copied3])
+  
+
+  // Hover Effect
+  const [isButtonVisible1, setIsButtonVisible1] = useState(false);
+  const [isButtonVisible2, setIsButtonVisible2] = useState(false);
+  const [isButtonVisible3, setIsButtonVisible3] = useState(false);
+
+  const handleMouseEnter1 = () => {
+    setIsButtonVisible1(true);
+  };
+
+  const handleMouseLeave1 = () => {
+    setIsButtonVisible1(false);
+  };
+
+  const handleMouseEnter2 = () => {
+    setIsButtonVisible2(true);
+  };
+
+  const handleMouseLeave2 = () => {
+    setIsButtonVisible2(false);
+  };
+
+  const handleMouseEnter3 = () => {
+    setIsButtonVisible3(true);
+  };
+
+  const handleMouseLeave3 = () => {
+    setIsButtonVisible3(false);
+  };
+  
   return (
-   <div className='cards'>
-      <div className = "card">
-        <h3 className='head'>Rectified</h3>
-        <div className="content">{/* API Call here */}</div>
+    <>
+      <div className='cards'>
+        <div className = "card">
+          <h3 className='head'>Rectified</h3>
+          <div onMouseEnter={handleMouseEnter1}
+               onMouseLeave={handleMouseLeave1} className="content">
+          {isButtonVisible1 && (
+            <div class="float">
+              <Clipboard
+          copied={copied1}
+          setCopied={setCopied1}
+          text= { rectified }
+          color='white'
+        /> </div>
+        )}
+        { rectified }
+          </div>
+        </div>
+
+        <div className = "card">
+          <h4 className='head'>Rephrased</h4>
+          <h4 className="sub-head">Concise, less wordy</h4>
+          <div onMouseEnter={handleMouseEnter2}
+               onMouseLeave={handleMouseLeave2} className="content">
+          {isButtonVisible2 && (
+            <div class="float">
+            <Clipboard
+            copied={copied2}
+            setCopied={setCopied2}
+            text= { concise }
+            color='white'
+          /></div>
+          )}
+        { concise }
+        </div>
+
+          <h4 className="sub-head">Clearer, more verbose</h4>
+          <div onMouseEnter={handleMouseEnter3}
+          onMouseLeave={handleMouseLeave3} className="content">
+
+          {isButtonVisible3 && (
+            <div class="float">
+            <Clipboard
+            copied={copied3}
+            setCopied={setCopied3}
+            text={ verbose }
+            color='white'
+          /></div>
+          )}
+        { verbose }
+        </div>
+
       </div>
 
-      <div className = "card">
-        <h4 className='head'>Rephrased</h4>
-        <h4 className="sub-head">Concise, less wordy</h4>
-        <div className="content">{/* API Call here */} ashdfiasdfaosd faisd fasdfasd fasd fd as dfas df asd fas dfa sdfads fds f asd fsad df asd This is just supposed to be an jasdf asd fasd fasdf asd fasd fasdf asdf asdf asd fsadf asdf dsf asdf hsdfu hsudf hsduhf duf hsufhsdf hsduf hsdufdhfsdu fhsduf sdhf dsuf hsfu hsdf usdhf sudfh sdfu hsdu ffh sudf asdf asd fas df asdf as df asd f asdf asdf a sdf a sd f asd f asdf as df asdf sad fas df asd</div>
-        <h4 className="sub-head">Clearer, more verbose</h4>
-        <div className="content">{/* API Call here */}</div>
-      </div>
+    <button onClick={handleClick}>Create</button>
+    <h1> Hello there, how has your wonderful day been today till now!</h1>
     </div>
-  )
+  </>
+)
 }
 
 export default App;
