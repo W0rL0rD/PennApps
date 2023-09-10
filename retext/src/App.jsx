012599545a07e8887e8ba216c3761a8d8455b3bd
Hetarth Mahida <hetarthmahida@gmail.com>
2023-09-10 00:00:18 -0400

    THis is pretty much final (no copy)

--- retext/src/App.jsx
+++ retext/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className = "card">
         <h4 className='head'>Rephrased</h4>
         <h4 className="sub-head">Concise, less wordy</h4>
-        <div className="content">{/* API Call here */}</div>
+        <div className="content">{/* API Call here */} ashdfiasdfaosd faisd fasdfasd fasd fd as dfas df asd fas dfa sdfads fds f asd fsad df asd This is just supposed to be an jasdf asd fasd fasdf asd fasd fasdf asdf asdf asd fsadf asdf dsf asdf hsdfu hsudf hsduhf duf hsufhsdf hsduf hsdufdhfsdu fhsduf sdhf dsuf hsfu hsdf usdhf sudfh sdfu hsdu ffh sudf asdf asd fas df asdf as df asd f asdf asdf a sdf a sd f asd f asdf as df asdf sad fas df asd</div>
         <h4 className="sub-head">Clearer, more verbose</h4>
         <div className="content">{/* API Call here */}</div>
       </div>
